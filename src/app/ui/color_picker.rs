6728e35230ef47719f88e32020c6201ede18efa9
Adonai Tewolde <adonai.tewolde@htwg-konstanz.de>
2026-07-11 06:15:59 +0200

    Refine Color Picker placement

--- src/app/ui/color_picker.rs
+++ src/app/ui/color_picker.rs
@@ -2,7 +2,8 @@ use egui::{Color32, Context};
 
 pub(super) fn show(context: &Context, color: &mut Color32) {
     egui::Window::new("Color picker")
-        .default_pos([12.0, 100.0])
+        .title_bar(false)
+        .anchor(egui::Align2::RIGHT_TOP, [0.0, 0.0])
         .resizable(false)
         .show(context, |ui| {
             ui.spacing_mut().slider_width = 275.0;
