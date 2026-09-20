a3bd9b65b60a1e7012211a361a00c529e38e0e7f
Adonai Tewolde <adonai.twld@gmail.com>
2026-09-20 09:40:54 +0200

    Update sketch brush test

--- src/config.rs
+++ src/config.rs
@@ -514,7 +514,7 @@ mod tests {
         assert_eq!(sketch.preset.pressure.min_size, 0.25);
         assert_eq!(sketch.preset.pressure.min_opacity, 0.01);
         assert_eq!(sketch.preset.pressure.full_opacity_pressure, 0.8);
-        assert_eq!(sketch.preset.pressure.opacity_gamma, 4.0);
+        assert_eq!(sketch.preset.pressure.opacity_gamma, 10.0);
         assert_eq!(rounded.preset.spacing.minimum, 0.5);
         assert_eq!(rectangle.preset.spacing.minimum, 0.5);
         assert_eq!(rounded.stamp_image.dimensions(), (128, 128));
